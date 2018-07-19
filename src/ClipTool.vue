843f170f4763fdf8f809272526b030010358376f
Žiga Pavlin <ziga.pavlin@futura.si>
2018-07-19 14:04:00 +0200

    no longer track points array internally

--- src/ClipTool.vue
+++ src/ClipTool.vue
@@ -1,14 +1,14 @@
 <template>
     <svg ref="svg" class="clip-tool" :width="width || '100%'" :height="height || '100%'" :viewBox="viewbox" :data-action="action" @click="addPoint" @contextmenu="onContextMenu">
-        <polygon class="clip-tool-path-outline" v-if="points.length > 0" ref="pathOutline" :points="path" stroke="transparent" :stroke-width="mOptions.strokeArea" fill="none"  @mouseenter="setPathHover" @mouseleave="setPathNormal">
+        <polygon class="clip-tool-path-outline" v-if="value.length > 0" ref="pathOutline" :points="path" stroke="transparent" :stroke-width="mOptions.strokeArea" fill="none"  @mouseenter="setPathHover" @mouseleave="setPathNormal">
             <title v-if="mOptions.pathTitle">{{mOptions.pathTitle}}</title>
         </polygon>
-        <polygon class="clip-tool-path-display" v-if="points.length > 0" ref="pathDisplay" :points="path" :stroke="mOptions.stroke" :stroke-width="mOptions.strokeWidth" fill="none"></polygon>
-        <g class="clip-tool-point" v-for="(point, i) of points" :key="i">
+        <polygon class="clip-tool-path-display" v-if="value.length > 0" ref="pathDisplay" :points="path" :stroke="mOptions.stroke" :stroke-width="mOptions.strokeWidth" fill="none"></polygon>
+        <g class="clip-tool-point" v-for="(point, i) of value" :key="i">
             <circle class="clip-tool-point-outline" :data-selected="i === selectedPoint" :index="i" :cx="point[0]" :cy="point[1]" :r="mOptions.strokeArea / 2" stroke="none" fill="transparent"><title v-if="mOptions.pointTitle">{{mOptions.pointTitle}}</title></circle>
             <circle class="clip-tool-point-display" :cx="point[0]" :cy="point[1]" :r="mOptions.strokeWidth" stroke="none" :fill="mOptions.stroke"></circle>
         </g>
-        <g v-if="mOptions.blankText && points.length === 0">
+        <g v-if="mOptions.blankText && value.length === 0">
             <text  class="clip-tool-text" text-anchor="middle" :x="width / 2" :y="height / 2" dy="7" stroke="white" stroke-width="4" :fill="mOptions.stroke">{{mOptions.blankText}}</text>
             <text  class="clip-tool-text" text-anchor="middle" :x="width / 2" :y="height / 2" dy="7" stroke="none" :fill="mOptions.stroke">{{mOptions.blankText}}</text>
         </g>
@@ -18,9 +18,9 @@
 <script>
     import Hammer from 'hammerjs';
 
-    const getPathLength = (points) => {
-        return points.reduce((sum, a, i) => {
-            const b = i + 1 === points.length ? points[0] : points[i + 1];
+    const getPathLength = (value) => {
+        return value.reduce((sum, a, i) => {
+            const b = i + 1 === value.length ? value[0] : value[i + 1];
 
             let dx = Math.abs(b[0] - a[0]),
                 dy = Math.abs(b[1] - a[1]),
@@ -103,7 +103,6 @@
                 selectedPoint: null,
                 disableAddPoint: false,
                 action: null,
-                points: this.value.slice(0, this.value.length),
                 managers: [],
                 pressManager: null,
                 isMobile: (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)))
@@ -128,10 +127,10 @@
                 return `0 0 ${this.width} ${this.height}`;
             },
             path() {
-                return this.points.map(x => x.join(',')).join(' ');
+                return this.value.map(x => x.join(',')).join(' ');
             },
             length() {
-                return getPathLength(this.points);
+                return getPathLength(this.value);
             }
         },
         mounted() {
@@ -151,7 +150,7 @@
             }
         },
         beforeDestroy() {
-            // remove listeners from points
+            // remove listeners from value
             this.managers.forEach(m => {this.removeListeners(m)});
             // remove listener for press
             if (this.pressManager) {
@@ -166,14 +165,14 @@
                     let clickPoint = this.$refs.svg.createSVGPoint();
                     clickPoint.x = e.layerX;
                     clickPoint.y = e.layerY;
-                    // check if target is polygon -> add new point between existing points
+                    // check if target is polygon -> add new point between existing value
                     if (this.$refs.pathOutline && this.$refs.pathOutline.isPointInStroke(clickPoint)) {
                         // make a copy of array length. It goes to infinitive loop if not
-                        const length = Number(this.points.length);
+                        const length = Number(this.value.length);
                         // try all positions of new point. If we have a right one, the length shouldn't change
                         for (let i = 1; i <= length; i++) {
                             // make a deep copy of array
-                            const newPoints = this.points.slice(0, this.points.length);
+                            const newPoints = this.value.slice(0, this.value.length);
                             // add new point
                             newPoints.splice(i, 0, [clickPoint.x, clickPoint.y]);
                             // calculate new length
@@ -181,11 +180,11 @@
                             // add point to index = i if length didn't change for more than this.
                             if (Math.ceil(newLength) <= Math.ceil(this.length)) {
                                 // find closest point on line
-                                const pointOnLine = getClosestPointOnLine([e.layerX, e.layerY], this.points[(i === 0 ? this.points.length : i) - 1], this.points[i === this.points.length ? 0 : i]);
-                                const prevPointDistance = Math.sqrt(Math.pow(Math.abs(this.points[(i === 0 ? this.points.length : 1) - 1][0] - pointOnLine[0]), 2) + Math.pow(Math.abs(this.points[(i === 0 ? this.points.length : 1) - 1][1] - pointOnLine[1]), 2));
-                                const nextPointDistance = Math.sqrt(Math.pow(Math.abs(this.points[i === this.points.length ? 0 : i][0] - pointOnLine[0]), 2) + Math.pow(Math.abs(this.points[i === this.points.length ? 0 : i][1] - pointOnLine[1]), 2));
+                                const pointOnLine = getClosestPointOnLine([e.layerX, e.layerY], this.value[(i === 0 ? this.value.length : i) - 1], this.value[i === this.value.length ? 0 : i]);
+                                const prevPointDistance = Math.sqrt(Math.pow(Math.abs(this.value[(i === 0 ? this.value.length : 1) - 1][0] - pointOnLine[0]), 2) + Math.pow(Math.abs(this.value[(i === 0 ? this.value.length : 1) - 1][1] - pointOnLine[1]), 2));
+                                const nextPointDistance = Math.sqrt(Math.pow(Math.abs(this.value[i === this.value.length ? 0 : i][0] - pointOnLine[0]), 2) + Math.pow(Math.abs(this.value[i === this.value.length ? 0 : i][1] - pointOnLine[1]), 2));
                                 if (prevPointDistance >= this.mOptions.minPointDistance && nextPointDistance >= this.mOptions.minPointDistance) {
-                                    this.points.splice(i, 0, pointOnLine.map(x => Math.round(x)));
+                                    this.value.splice(i, 0, pointOnLine.map(x => Math.round(x)));
                                     this.endEdit();
                                 }
                                 break;
@@ -198,9 +197,9 @@
                         let left = e.layerX;
                         let top = e.layerY;
                         // if shift key is pressed we assume user wants to put point in straight line
-                        if (e.shiftKey && this.points.length !== 0) {
+                        if (e.shiftKey && this.value.length !== 0) {
                             // get last item value
-                            const lastPoint = this.points.slice(-1)[0];
+                            const lastPoint = this.value.slice(-1)[0];
 
                             // check which diff is smaller
                             const dx = Math.abs(left - lastPoint[0]);
@@ -212,7 +211,7 @@
                                 left = lastPoint[0];
                             }
                         }
-                        this.points.push([left, top].map(x => Math.round(x)));
+                        this.value.push([left, top].map(x => Math.round(x)));
                         this.endEdit();
                     }
                 }
@@ -245,15 +244,15 @@
             panMove(e) {
                 if (!this.selectedPoint) {
                     const index = e.target.attributes.index.value;
-                    const x = this.points[index][0] + e.srcEvent.movementX;
-                    const y = this.points[index][1] + e.srcEvent.movementY;
+                    const x = this.value[index][0] + e.srcEvent.movementX;
+                    const y = this.value[index][1] + e.srcEvent.movementY;
                     if (this.mOptions.limitToParent) {
                         if (x > (this.mOptions.strokeWidth * 2) && x < this.width - (this.mOptions.strokeWidth * 2) && y > (this.mOptions.strokeWidth * 2) && y < this.height - (this.mOptions.strokeWidth * 2)) {
-                            this.points.splice(index, 1, [x, y]);
+                            this.value.splice(index, 1, [x, y]);
                         }
                     }
                     else {
-                        this.points.splice(index, 1, [x, y]);
+                        this.value.splice(index, 1, [x, y]);
                     }
                 }
             },
@@ -294,7 +293,7 @@
                 if (e.key === 'Backspace' || e.key === 'Delete') {
                     if (this.mOptions.confirmText && !confirm(this.mOptions.confirmText)) return;
                     e.preventDefault();
-                    this.points.splice(this.selectedPoint, 1);
+                    this.value.splice(this.selectedPoint, 1);
                 }
                 this.action = null;
                 this.selectedPoint = null;
@@ -308,7 +307,7 @@
             },
             endEdit() {
                 // check if num of point changed
-                if (this.managers.length !== this.points.length) {
+                if (this.managers.length !== this.value.length) {
                     // reset managers
                     setTimeout(function () {
                         this.managers.forEach(manager => manager.destroy());
@@ -318,7 +317,7 @@
                         });
                     }.bind(this), 100);
                 }
-                this.$emit('input', this.points);
+                this.$emit('input', this.value);
                 this.$emit('change');
             }
         }
